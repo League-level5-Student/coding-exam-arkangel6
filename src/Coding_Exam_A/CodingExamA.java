f98fada83b0d414b51fb4f480a0efa6d161febac
arkangel6 <league@WTS14.attlocal.net>
2019-01-28 19:13:56 -0800

    finished 1/28/19

--- src/Coding_Exam_A/CodingExamA.java
+++ src/Coding_Exam_A/CodingExamA.java
@@ -1,6 +1,8 @@
 package Coding_Exam_A;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.JOptionPane;
 
@@ -9,18 +11,69 @@ import org.jointheleague.graphical.robot.Robot;
 public class CodingExamA {
 	public static void main(String[] args) {
 		/*
-		 * Write a program that asks the user for three pieces of information.
-		 * 1. How many robots
-		 * 2. The color of the shapes
-		 * 3. How many sides each shape will have
+		 * Write a program that asks the user for three pieces of information. 1. How
+		 * many robots 2. The color of the shapes 3. How many sides each shape will have
 		 * 
-		 * Once the information has been collected, the program will then make the requested number of robots
-		 * each draw the requested shape in the requested color. The robots should execute at the same time so 
-		 * Threads will need to be used. Arrange the robots so that the shapes do not overlap.
-		 * For full credit, define the Thread functions using lambdas.
+		 * Once the information has been collected, the program will then make the
+		 * requested number of robots each draw the requested shape in the requested
+		 * color. The robots should execute at the same time so Threads will need to be
+		 * used. Arrange the robots so that the shapes do not overlap. For full credit,
+		 * define the Thread functions using lambdas.
 		 * 
-		 * See the Coding_Exam_A_Demo.jar for an example of what the finished product should look like.
+		 * See the Coding_Exam_A_Demo.jar for an example of what the finished product
+		 * should look like.
 		 */
+
+		String robotNum = JOptionPane.showInputDialog("how many robots?");
+		int robNum = Integer.parseInt(robotNum);
+
+		String color = JOptionPane.showInputDialog("what color are the shapes?");
+		HashMap<String, Color> c = new HashMap<String, Color>();
+		c.put("blue", Color.blue);
+		c.put("red", Color.red);
+		c.put("green", Color.green);
+		c.put("yellow", Color.yellow);
+		c.put("pink", Color.pink);
+		c.put("orange", Color.orange);
+		c.put("magenta", Color.magenta);
+
+		Color c2 = c.get(0);
+		for (int i = 0; i < c.size(); i++) {
+			if (c.containsKey(color)) {
+				c2 = c.get(color);
+			}
+		}
+
+		String sides = JOptionPane.showInputDialog("how many sides are the shapes?");
+		int sideNum = Integer.parseInt(sides);
+		int angle = 180 * (sideNum - 2) / sideNum;
+
+		System.out.println(angle);
+
+		ArrayList<Robot> robList = new ArrayList<Robot>();
+		int x = 0;
+		for (int i = 0; i < robNum; i++) {
+			Robot r = new Robot(200 + x, 200);
+			r.setSpeed(10);
+			r.penDown();
+			r.setPenColor(c2);
+			robList.add(r);
+			x += 200;
+		}
+
+		for (int i = 0; i < robNum; i++) {
+
+			Robot k = robList.get(i);
+			Thread t1 = new Thread(() -> {
+				for (int j = 0; j < sideNum; j++) {
+					k.move(150);
+					k.turn(180 - angle);
+				}
+
+			});
+
+			t1.start();
+		}
 
 	}
 }
--- src/Coding_Exam_B/CodingExamB.java
+++ src/Coding_Exam_B/CodingExamB.java
@@ -8,35 +8,72 @@ import java.io.IOException;
 
 public class CodingExamB {
 	/*
-	 * This is a logging program for collecting TODO comments in a program. The program will scan
-	 * Through all the files in the Coding_Exam_B/classes package, and collect all the comments that start
-	 * with //TODO: and write them to their own file. See the TODO_Log_example.txt file for an idea of what 
-	 * the final file output will look like.
+	 * This is a logging program for collecting TODO comments in a program. The
+	 * program will scan Through all the files in the Coding_Exam_B/classes package,
+	 * and collect all the comments that start with //TODO: and write them to their
+	 * own file. See the TODO_Log_example.txt file for an idea of what the final
+	 * file output will look like.
 	 */
-	
-	
+
 	public static String getLoggingInfo(String fileName) {
 		/*
-		 * 1. Complete the getLoggingInfoMethod.
-		 *    The method takes in a String for a file and returns a String. 
-		 *    The method will open the file a read through it. It will then 
-		 *    take all the comments that begin with //TODO: and combine them 
-		 *    into one large String. The string will also state the file name and
-		 *    the line number for where each TODO was found. 
-		*/
-		
-		return "";
+		 * 1. Complete the getLoggingInfoMethod. The method takes in a String for a file
+		 * and returns a String. The method will open the file a read through it. It
+		 * will then take all the comments that begin with //TODO: and combine them into
+		 * one large String. The string will also state the file name and the line
+		 * number for where each TODO was found.
+		 */
+		String all = "";
+		int counter = 0;
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(fileName));
+
+			String line = br.readLine();
+			counter = 1;
+			all += fileName + "\n";
+			while (line != null) {
+
+				if (line.contains("//TODO")) {
+					all += counter + ":  " + line.trim() + "\n";
+
+				}
+				counter++;
+				line = br.readLine();
+			}
+
+			all += "\n";
+
+			br.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return all;
 	}
-	
+
 	public static void main(String[] args) {
 		String finalLogString = getLoggingInfo("src/Coding_Exam_B/classes/Camera.java");
 		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/RayTracedImageViewer.java");
 		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/RayTracer.java");
 		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/Vector3.java");
-		
+
 		/*
-		 * 2. Write the finalLogString to a file called TODO_Log.txt. The file should match TODO_Log_example.txt. 
+		 * 2. Write the finalLogString to a file called TODO_Log.txt. The file should
+		 * match TODO_Log_example.txt.
 		 */
+		System.out.println(finalLogString);
+		try {
+			FileWriter fw = new FileWriter("src/Coding_Exam_B/TODO_Log.txt");
+			fw.write(finalLogString);
+			fw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 }
